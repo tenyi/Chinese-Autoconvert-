d188fccf38542e79f8227f9af9da2eb1e0713ebf
曾騰毅 <tenyi@sinotech.org.tw>
2020-07-14 11:45:20 +0800

    發現 hanziconv 有過度換詞的問題，換回使用 Jianfan

---
+++ jianfan/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='jianfan',
+      version='0.0.2',
+      description='Jianfan is a library for translation between traditional and simplified chinese',
+      author='Leon Dong',
+      author_email='Leon.Dong@gmail.com',
+      url=' http://code.google.com/p/python-jianfan/',
+      packages=find_packages(),
+      long_description="Jianfan is a library for translation between traditional and simplified chinese.",
+      license="GPL",
+      platforms=["any"],
+     )
